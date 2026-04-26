ff80764887a045fea9f01d75c8382cad3f069027
AlexeyFrens <leiloka46@gmail.com>
2026-04-25 22:25:50 -0300

    feat: Layout do LoginScreen finalizado

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
 import {LoginScreen} from "./src/screens/LoginScreen";
+import {SignUpScreen} from "./src/screens/SignUpScreen";
 
 const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name={"login"} component={LoginScreen}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name={"login"} component={LoginScreen}/>
+                <Stack.Screen name={"signUp"} component={SignUpScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,18 +1,45 @@
-import {Image, Text, TextInput, View, StyleSheet} from "react-native";
+import {Image, Text, TextInput, View, StyleSheet, TouchableOpacity} from "react-native";
 import {images} from "../../assets/ImageStorage";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {useNavigation} from "@react-navigation/native";
+
+//TODO Inserir fonte personalizada
 
 export const LoginScreen = () => {
+
+    const navigation = useNavigation();
 
     return (
         <>
             <SafeAreaProvider>
-                <SafeAreaView>
+                <SafeAreaView style={styles.container}>
                     <Image source={images.brandIcon} resizeMode={"contain"}/>
 
-                    <Text>Login</Text>
+                    <Text style={styles.pageTitle}>Login</Text>
 
-                    <TextInput placeholder={"Ex: fulano@gmail.com"} style={styles.input}/>
+                    <View style={{gap: 20, width: '100%'}}>
+                        <View style={{gap: 5}}>
+                            <Text style={styles.text}>Email</Text>
+                            <TextInput placeholder={"Ex: fulano@gmail.com"} style={styles.input}
+                                       keyboardType={"email-address"} placeholderTextColor={"#B0BEC5"} autoCapitalize={"none"}/>
+                        </View>
+                        <View style={{gap: 5}}>
+                            <Text style={styles.text}>Senha</Text>
+                            <TextInput placeholder={"Digite sua senha"} style={styles.input}
+                                       placeholderTextColor={"#B0BEC5"}/>
+                        </View>
+
+                        <Text style={styles.link}>Esqueci minha senha</Text>
+
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}>Acessar</Text>
+                        </TouchableOpacity>
+
+                        <View style={{flexDirection: "row", gap: 5}}>
+                            <Text style={styles.text}>Ainda não possui uma conta?</Text>
+                            <Text style={styles.link} onPress={() => {navigation.navigate('signUp')}}>Inscreva-se</Text>
+                        </View>
+                    </View>
                 </SafeAreaView>
             </SafeAreaProvider>
         </>
@@ -20,10 +47,47 @@ export const LoginScreen = () => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyItems: 'center',
+        alignItems: 'center',
+        padding: 45,
+        backgroundColor: '#313131',
+        gap: 45
+    },
     input: {
-        borderWidth: 1,
+        borderWidth: 2,
         padding: 5,
         borderRadius: 5,
-        borderColor: "#585858"
+        borderColor: "#585858",
+        width: '100%',
+        maxWidth: 322,
+        fontSize: 12,
+        color: "#B0BEC5"
+    },
+    pageTitle: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: '#FFFFFF'
+    },
+    text: {
+        fontSize: 12,
+        color: '#FFFFFF'
+    },
+    link: {
+        fontSize: 12,
+        color: '#FFFFFF',
+        textDecorationLine: 'underline'
+    },
+    button: {
+        backgroundColor: '#2962FF',
+        borderRadius: 15,
+        padding: 10
+    },
+    buttonText: {
+        fontSize: 15,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 })
