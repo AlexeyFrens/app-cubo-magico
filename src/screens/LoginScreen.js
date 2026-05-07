3f96959646ca935ac01d2e9f342401742f94ca46
AlexeyFrens <leiloka46@gmail.com>
2026-05-06 22:38:21 -0300

    feat: Resolvendo Problemas de UX Envolvendo o Teclado e Input

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,13 @@
-import {Image, Text, TextInput, View, StyleSheet, TouchableOpacity, ActivityIndicator} from "react-native";
+import {
+    Image,
+    Text,
+    TextInput,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    ActivityIndicator,
+    KeyboardAvoidingView, Platform, ScrollView, Pressable, Keyboard
+} from "react-native";
 import {images} from "../../assets/ImageStorage";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {useNavigation} from "@react-navigation/native";
@@ -37,42 +46,50 @@ export const LoginScreen = () => {
     return (
         <>
             <SafeAreaProvider>
-                <SafeAreaView style={styles.mainContainer}>
-                    <Image source={images.brandIcon} resizeMode={"contain"}/>
+                <SafeAreaView style={{flex: 1, backgroundColor: colors.backgroundPrimary}}>
+                    <KeyboardAvoidingView style={{ flex: 1, width: '100%'}}
+                                          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    >
+                        <ScrollView keyboardShouldPersistTaps={'handled'} showsHorizontalScrollIndicator={false}>
+                            <Pressable style={styles.mainContainer} onPress={Keyboard.dismiss}>
+                                <Image source={images.brandIcon} resizeMode={"contain"}/>
 
-                    <Text style={styles.pageTitle}>Login</Text>
+                                <Text style={styles.pageTitle}>Login</Text>
 
-                    <View style={{gap: 20, width: '100%'}}>
-                        <View style={{gap: 5}}>
-                            <Text style={styles.text}>Email</Text>
-                            <TextInput placeholder={"Ex: fulano@gmail.com"} style={styles.input}
-                                       keyboardType={"email-address"} placeholderTextColor={colors.textPlaceholder}
-                                       autoCapitalize={"none"} onChangeText={setEmail}/>
-                        </View>
-                        <View style={{gap: 5}}>
-                            <Text style={styles.text}>Senha</Text>
-                            <TextInput placeholder={"Digite sua senha"} style={styles.input}
-                                       placeholderTextColor={colors.textPlaceholder}
-                                       secureTextEntry={true} autoCapitalize={"none"} onChangeText={setPassword}/>
-                        </View>
+                                <View style={{gap: 20, width: '100%'}}>
+                                    <View style={{gap: 5}}>
+                                        <Text style={styles.text}>Email</Text>
+                                        <TextInput placeholder={"Ex: fulano@gmail.com"} style={styles.input}
+                                                   keyboardType={"email-address"} placeholderTextColor={colors.textPlaceholder}
+                                                   autoCapitalize={"none"} onChangeText={setEmail}/>
+                                    </View>
+                                    <View style={{gap: 5}}>
+                                        <Text style={styles.text}>Senha</Text>
+                                        <TextInput placeholder={"Digite sua senha"} style={styles.input}
+                                                   placeholderTextColor={colors.textPlaceholder}
+                                                   secureTextEntry={true} autoCapitalize={"none"} onChangeText={setPassword}/>
+                                    </View>
 
-                        <Text style={styles.link}>Esqueci minha senha</Text>
+                                    <Text style={styles.link}>Esqueci minha senha</Text>
 
-                        <TouchableOpacity style={styles.button} onPress={signIn} disabled={isLoading}>
-                            {isLoading ? (
-                                <ActivityIndicator color="white"/>
-                            ) : (
-                                <Text style={styles.buttonText}>Acessar</Text>
-                            )}
-                        </TouchableOpacity>
+                                    <TouchableOpacity style={styles.button} onPress={signIn} disabled={isLoading}>
+                                        {isLoading ? (
+                                            <ActivityIndicator color="white"/>
+                                        ) : (
+                                            <Text style={styles.buttonText}>Acessar</Text>
+                                        )}
+                                    </TouchableOpacity>
 
-                        <View style={{flexDirection: "row", gap: 5}}>
-                            <Text style={styles.text}>Ainda não possui uma conta?</Text>
-                            <Text style={styles.link} onPress={() => {
-                                navigation.navigate('signUp')
-                            }}>Inscreva-se</Text>
-                        </View>
-                    </View>
+                                    <View style={{flexDirection: "row", gap: 5}}>
+                                        <Text style={styles.text}>Ainda não possui uma conta?</Text>
+                                        <Text style={styles.link} onPress={() => {
+                                            navigation.navigate('signUp')
+                                        }}>Inscreva-se</Text>
+                                    </View>
+                                </View>
+                            </Pressable>
+                        </ScrollView>
+                    </KeyboardAvoidingView>
                 </SafeAreaView>
             </SafeAreaProvider>
         </>
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,4 +1,17 @@
-import {ActivityIndicator, Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {
+    ActivityIndicator,
+    Image,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    Pressable,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from "react-native";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 import {images} from "../../assets/ImageStorage";
 import {useNavigation} from "@react-navigation/native";
@@ -48,45 +61,56 @@ export const SignUpScreen = () => {
 
     return (
         <SafeAreaProvider>
-            <SafeAreaView style={styles.mainContainer}>
-                <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-                    <Image source={images.backButton} resizeMode={"contain"}/>
-                    <Text style={styles.backButtonText}>Voltar</Text>
-                </TouchableOpacity>
+            <SafeAreaView style={{flex: 1, backgroundColor: colors.backgroundPrimary}}>
+                <KeyboardAvoidingView style={{flex: 1, width: '100%'}}
+                                      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                >
+                    <ScrollView keyboardShouldPersistTaps={'handled'} showsHorizontalScrollIndicator={false}>
+                        <Pressable style={styles.mainContainer} onPress={Keyboard.dismiss}>
+                            <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+                                <Image source={images.backButton} resizeMode={"contain"}/>
+                                <Text style={styles.backButtonText}>Voltar</Text>
+                            </TouchableOpacity>
 
-                <Image source={images.brandIcon} resizeMode={"contain"}/>
+                            <Image source={images.brandIcon} resizeMode={"contain"}/>
 
-                <Text style={styles.pageTitle}>Inscreva-se</Text>
+                            <Text style={styles.pageTitle}>Inscreva-se</Text>
 
-                <View style={{gap: 20, width: '100%'}}>
-                    <View style={{gap: 5}}>
-                        <Text style={styles.text}>Email</Text>
-                        <TextInput placeholder={"Ex: fulano@gmail.com"} style={styles.input}
-                                   keyboardType={"email-address"} placeholderTextColor={colors.textPlaceholder}
-                                   autoCapitalize={"none"} onChangeText={setEmail}/>
-                    </View>
-                    <View style={{gap: 5}}>
-                        <Text style={styles.text}>Senha</Text>
-                        <TextInput placeholder={"Digite uma senha"} style={styles.input}
-                                   placeholderTextColor={colors.textPlaceholder}
-                                   secureTextEntry={true} autoCapitalize={"none"} onChangeText={setPassword}/>
-                    </View>
+                            <View style={{gap: 20, width: '100%'}}>
+                                <View style={{gap: 5}}>
+                                    <Text style={styles.text}>Email</Text>
+                                    <TextInput placeholder={"Ex: fulano@gmail.com"} style={styles.input}
+                                               keyboardType={"email-address"}
+                                               placeholderTextColor={colors.textPlaceholder}
+                                               autoCapitalize={"none"} onChangeText={setEmail}/>
+                                </View>
+                                <View style={{gap: 5}}>
+                                    <Text style={styles.text}>Senha</Text>
+                                    <TextInput placeholder={"Digite uma senha"} style={styles.input}
+                                               placeholderTextColor={colors.textPlaceholder}
+                                               secureTextEntry={true} autoCapitalize={"none"}
+                                               onChangeText={setPassword}/>
+                                </View>
 
-                    <View style={{gap: 5}}>
-                        <Text style={styles.text}>Confirmar Senha</Text>
-                        <TextInput placeholder={"Digite novamente a senha"} style={styles.input}
-                                   placeholderTextColor={colors.textPlaceholder}
-                                   secureTextEntry={true} autoCapitalize={"none"} onChangeText={setConfirmPassword}/>
-                    </View>
+                                <View style={{gap: 5}}>
+                                    <Text style={styles.text}>Confirmar Senha</Text>
+                                    <TextInput placeholder={"Digite novamente a senha"} style={styles.input}
+                                               placeholderTextColor={colors.textPlaceholder}
+                                               secureTextEntry={true} autoCapitalize={"none"}
+                                               onChangeText={setConfirmPassword}/>
+                                </View>
 
-                    <TouchableOpacity style={styles.button} onPress={signUp} disabled={isLoading}>
-                        {isLoading ? (
-                            <ActivityIndicator color="white" />
-                        ) : (
-                            <Text style={styles.buttonText}>Inscrever</Text>
-                        )}
-                    </TouchableOpacity>
-                </View>
+                                <TouchableOpacity style={styles.button} onPress={signUp} disabled={isLoading}>
+                                    {isLoading ? (
+                                        <ActivityIndicator color="white"/>
+                                    ) : (
+                                        <Text style={styles.buttonText}>Inscrever</Text>
+                                    )}
+                                </TouchableOpacity>
+                            </View>
+                        </Pressable>
+                    </ScrollView>
+                </KeyboardAvoidingView>
             </SafeAreaView>
         </SafeAreaProvider>
     )
