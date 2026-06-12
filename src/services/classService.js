c96ef52be02084432d6d46c813d1bd4a46fbc89e
AlexeyFrens <leiloka46@gmail.com>
2026-06-11 22:31:01 -0300

    fix: Removendo Try Catch Conflitante

--- src/services/classService.js
+++ src/services/classService.js
@@ -2,21 +2,16 @@ import {supabase} from "./supabaseConnection";
 
 export const classService = {
     searchStapsByTrail: async (trailId) => {
-        try {
-            const {data, error} = await supabase
-                .from('etapas')
-                .select('*')
-                .eq('trilha_id', trailId)
-                .order('ordem', {ascending: true})
+        const {data, error} = await supabase
+            .from('etapas')
+            .select('*')
+            .eq('trilha_id', trailId)
+            .order('ordem', {ascending: true})
 
-            if (error) {
-                throw new Error(error.message)
-            }
-
-            return data
-        } catch (error) {
-            console.log("Erro na Busca de Aulas: ", error.message)
-            throw error
+        if (error) {
+            throw new Error(error.message)
         }
+
+        return data
     }
 }
